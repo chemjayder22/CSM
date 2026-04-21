83d3951125a3de6bb4d4df912099e7dc7220b31e
chemjayder22 <chemjayder.cabungcal@deped.gov.ph>
2026-04-21 16:10:26 +0800

    complete

--- signatories.js
+++ signatories.js
@@ -2,7 +2,7 @@
   // Global configuration for PDF signatories.
   // Edit these values to change the names/titles in the PDF.
   window.SDO_SIGNATORIES = {
-    preparedByName: 'CHEM JAYDER M. CABUNGCAL',
+    preparedByName: 'NOEL G. SEQUITO, Ed.D.',
     preparedByTitle: 'Information Technology Officer I',
     notedByName: 'CHRISTOPHER R. DIAZ, CESO V',
     notedByTitle: 'Schools Division Superintendent',
